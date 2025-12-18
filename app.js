d06fd1761e97a6a72c8f0b4f72b7a27cf9f7af1b
Coty Arturo <coti_arturo@hotmail.com>
2025-12-18 14:53:45 -0600

    otro cambio en las variables internas y de railway

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { wrapError, errorMiddleware } = require('./utils/error-middleware')
 const authMiddleware = require('./utils/auth-middleware')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 app.use(bodyParser.json())
 app.use(authMiddleware)
