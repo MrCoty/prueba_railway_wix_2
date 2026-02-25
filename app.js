fe5de5511a14a87547edeb1940a58486cf55270f
MrCoty <47020090+MrCoty@users.noreply.github.com>
2026-02-25 10:02:12 -0600

    estoy ponendo health para el path

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const authMiddleware = require('./utils/auth-middleware')
 const app = express()
 const port = process.env.PORT || 3000;
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
 app.use(bodyParser.json())
 app.use(authMiddleware)
 
