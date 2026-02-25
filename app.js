c1e808f84968dfac59a060efa5430b4072af0d8f
MrCoty <47020090+MrCoty@users.noreply.github.com>
2026-02-25 05:28:52 -0600

    5.7 ahora le hago caso al otro

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { wrapError, errorMiddleware } = require('./utils/error-middleware')
 const authMiddleware = require('./utils/auth-middleware')
 
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
 app.use(authMiddleware)
